4576fed67cf4c97a167a5ee475e82bbbd1de56bb
Gabryel Araújo <gabryelaraujo71@gmail.com>
2026-05-27 09:49:26 -0300

    feat: adiciona validação de código do dia e regras associadas

--- src/test/java/com/psn_cloud/psiconotes/services/disponibilidade/DisponibilidadeServiceImplTest.java
+++ src/test/java/com/psn_cloud/psiconotes/services/disponibilidade/DisponibilidadeServiceImplTest.java
@@ -3,9 +3,9 @@ package com.psn_cloud.psiconotes.services.disponibilidade;
 import com.psn_cloud.psiconotes.domain.Disponibilidade;
 import com.psn_cloud.psiconotes.domain.Psicologo;
 import com.psn_cloud.psiconotes.exceptions.RecursoNaoEncontradoException;
+import com.psn_cloud.psiconotes.exceptions.RegraDeNegocioException;
 import com.psn_cloud.psiconotes.repositories.DisponibilidadeRepository;
 import com.psn_cloud.psiconotes.repositories.PsicologoRepository;
-import org.assertj.core.api.ThrowableAssert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -55,17 +55,17 @@ class DisponibilidadeServiceImplTest {
         disponibilidade.setId(UUID.randomUUID());
         disponibilidade.setCodigoDia(1);
         disponibilidade.setDiaSemana("Segunda-Feira");
-        disponibilidade.setHoraInicio(LocalTime.of(8,0));
-        disponibilidade.setHoraFim(LocalTime.of(18,0));
+        disponibilidade.setHoraInicio(LocalTime.of(8, 0));
+        disponibilidade.setHoraFim(LocalTime.of(18, 0));
         disponibilidade.setAtivo(true);
     }
 
     @Test
-    void deve_criar_com_sucesso(){
+    void deve_criar_com_sucesso() {
         //arrange
         when(psicologoRepository.findById(psicologoAtivo.getId())).thenReturn(Optional.of(psicologoAtivo));
         when(disponibilidadeRepository.countByPsicologoId(psicologoAtivo.getId())).thenReturn(0L);
-        when(disponibilidadeRepository.existsByPsicologoIdAndCodigoDia(psicologoAtivo.getId(),disponibilidade.getCodigoDia())).thenReturn(false);
+        when(disponibilidadeRepository.existsByPsicologoIdAndCodigoDia(psicologoAtivo.getId(), disponibilidade.getCodigoDia())).thenReturn(false);
         when(disponibilidadeRepository.save(disponibilidade)).thenReturn(disponibilidade);
 
         //act
@@ -77,7 +77,7 @@ class DisponibilidadeServiceImplTest {
     }
 
     @Test
-    void deve_lancar_excecao_quando_psicolgo_nao_encontrado(){
+    void deve_lancar_excecao_quando_psicolgo_nao_encontrado() {
         //arrange
         when(psicologoRepository.findById(psicologoAtivo.getId())).thenReturn(Optional.empty());
 
@@ -86,4 +86,57 @@ class DisponibilidadeServiceImplTest {
                 .isInstanceOf(RecursoNaoEncontradoException.class)
                 .hasMessage("Psicólogo não encontrado");
     }
+
+    @Test
+    void deve_lancar_excecao_quando_psicologo_inativo() {
+        //arrange
+        psicologoAtivo.setAtivo(false);
+
+        when(psicologoRepository.findById(psicologoAtivo.getId())).thenReturn(Optional.of(psicologoAtivo));
+
+        //act & assert
+        assertThatThrownBy(() -> disponibilidadeServiceImpl.criar(disponibilidade))
+                .isInstanceOf(RegraDeNegocioException.class)
+                .hasMessage("Somente psicólogos ativos podem possuir disponibilidade");
+    }
+
+    @Test
+    void deve_lancar_excecao_quando_codigo_dia_for_invalido() {
+        //arrange
+        disponibilidade.setCodigoDia(8);
+
+        when(psicologoRepository.findById(psicologoAtivo.getId())).thenReturn(Optional.of(psicologoAtivo));
+
+        //act & assert
+        assertThatThrownBy(() -> disponibilidadeServiceImpl.criar(disponibilidade))
+                .isInstanceOf(RegraDeNegocioException.class)
+                .hasMessage("Código do dia inválido. Escolha um número entre 1 e 7");
+
+    }
+
+    @Test
+    void deve_lancar_excecao_quando_numero_de_disponibilidades_atingir_sete() {
+        //arrange
+        when(psicologoRepository.findById(psicologoAtivo.getId())).thenReturn(Optional.of(psicologoAtivo));
+        when(disponibilidadeRepository.countByPsicologoId(psicologoAtivo.getId())).thenReturn(7L);
+
+        //act & assert
+        assertThatThrownBy(() -> disponibilidadeServiceImpl.criar(disponibilidade))
+                .isInstanceOf(RegraDeNegocioException.class)
+                .hasMessage("Só é possível adicionar uma disponibilidade para cada dia da semana");
+    }
+
+    @Test
+    void deve_lancar_excecao_quando_disponibilidade_ja_cadastrada(){
+        //arrange
+        when(psicologoRepository.findById(psicologoAtivo.getId())).thenReturn(Optional.of(psicologoAtivo));
+        when(disponibilidadeRepository.countByPsicologoId(psicologoAtivo.getId())).thenReturn(3L);
+        when(disponibilidadeRepository.existsByPsicologoIdAndCodigoDia(psicologoAtivo.getId(), disponibilidade.getCodigoDia())).thenReturn(true);
+
+        //act & assert
+        assertThatThrownBy(() -> disponibilidadeServiceImpl.criar(disponibilidade))
+                .isInstanceOf(RegraDeNegocioException.class)
+                .hasMessage("Psicólogo já possui disponibilidade cadastrada para esse dia");
+    }
+
 }
